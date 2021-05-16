e9e16e645b73f9c619bc1acf07c99c6adcb6c268
Noah Olmeijer <n.c.c.p.olmeijer@student.tue.nl>
2021-05-16 21:12:00 +0200

    Aanpassingen gemaakt d.m.v. feedback 16/05/2021.

--- expressie_clusters.py
+++ expressie_clusters.py
@@ -27,47 +27,27 @@ def expressie(cluster_invoer, cluster_uitvoer, cluster_nummer=0):
 
     Formaat: meetmoment op x-as, relatieve expressiewaarde op y-as.
     """
-    # Inlezen van 'cluster_invoer' en
-    # rauwe data uit 'cluster_invoer' toewijzen aan
-    # variable 'cluster_invoer'.
-
+    # inlezen van databestanden 'cluster_invoer' en 'cluster_resultaat'.
     with open(cluster_invoer) as cluster_invoer:
         cluster_invoer_data = cluster_invoer.read().split()
-
-    # Inlezen van 'cluster_uitvoer' en
-    # rauwe data uit 'cluster_uitvoer' toewijzen aan
-    # variable 'cluster_uitvoer'.
+        cloneID_lijst = list(map(int, cluster_invoer_data[::9]))
 
     with open(cluster_resultaat) as cluster_uitvoer:
         cluster_uitvoer = cluster_uitvoer.read().split()
+        cluster_uitvoer_data = list(map(int, cluster_uitvoer))
 
-    # lijst aanmaken genaamd 'cluster_uitvoer_data' met per cloneID de
-    # corresponderende clusters.
-
-    cluster_uitvoer_data = list(map(int, cluster_uitvoer))
-
-    # lijst aanmaken genaamd 'cloneID_lijst' met alle cloneIDs op volgorde die
-    # is aangeleverd in 'cluster_invoer_data'.
-
-    cloneID_lijst = list(map(int, cluster_invoer_data[::9]))
-       
-    # verander str in int of float afhankelijk van het nummertype.
-
+    # verander string in integer of float, afhankelijk van het nummertype.
     for nummer in range(len(cluster_invoer_data)):
         if cluster_invoer_data[nummer] not in cloneID_lijst:
             cluster_invoer_data[nummer] = float(cluster_invoer_data[nummer])
 
     # lijst aanmaken genaamd 'cluster_lijst' die de clusters bevatten.
-
     cluster_lijst = cluster_uitvoer_data[1::2]
-
-    # lege dictionary aanmaken genaamd 'cloneID_dict'
 
     cloneID_dict = {}
 
     # voeg alle cloneIDs toe aan de corresponderende cluster in de dictionary
     # genaamd 'cloneID_dict'.
-
     for cluster in range(len(cluster_lijst)):
         if cluster_lijst[cluster] not in cloneID_dict:
             cloneID_dict[cluster_lijst[cluster]] = [cloneID_lijst[cluster]]
@@ -75,13 +55,11 @@ def expressie(cluster_invoer, cluster_uitvoer, cluster_nummer=0):
             cloneID_dict[cluster_lijst[cluster]].append(
                 cloneID_lijst[cluster])
 
-    # genereer een lijst met integers 1 tot en met 8.
-
+    # genereer een lijst met integers 1 t/m 8 voor de plot.
     x_axis = list(range(1, 9))
 
     # plot alle meetwaarden van de cloneIDs in een bepaalde cluster en
     # stel de plottitel en astitels in.
-
     for nummer in cloneID_dict[cluster_nummer]:
         ind = cluster_invoer_data.index(nummer)
         plt.plot(x_axis, cluster_invoer_data[ind+1:ind+9], '-x')
@@ -90,20 +68,17 @@ def expressie(cluster_invoer, cluster_uitvoer, cluster_nummer=0):
     plt.title("Cluster" + " " + str(cluster_nummer))
 
     # sla de gegenereerde plot op in map Cluster_Plots.
-
     plt.savefig("Cluster_Plots/Cluster_"
                 + str(cluster_nummer)
-                + ".png", dpi=300)
+                + ".png", dpi=200)
 
     # geef de plot weer in de IDE.
-
     plt.show()
 
 
 cluster_invoer = "Data/Voorbeeld_clusterdata.txt"
 cluster_resultaat = "Data/Voorbeeld_clusterresult.txt"
 
-# aanroepen van functie expressie() voor alle 6 clusters range(6)
-
+# aanroepen van functie expressie() voor alle 6 clusters.
 for i in range(6):
     expressie(cluster_invoer, cluster_resultaat, i)
