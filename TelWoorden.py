94bc05aab1c6e7fa17b79696c1bad49e2a882dc4
Noah Olmeijer <n.c.c.p.olmeijer@student.tue.nl>
2021-05-17 16:46:33 +0200

    Aanpassing van leesbaarheid + code uit de nieuwe versie die rekening houdt met symbolen in een woord.

--- TelWoorden.py
+++ TelWoorden.py
@@ -27,29 +27,29 @@ def TelWoorden(bestandsnaam):
     """
     # inlezen van invoerbestand en
     # rauwe data uit invoerbestand toewijzen aan variable 'data'.
-
     with open(bestandsnaam) as tekst_bestand:
         data = tekst_bestand.readlines()
 
-    # lijst aanmaken genaamd 'items' van alle losse woorden
-    # en integers uit 'data' zonder komma's.
+    # lijst aanmaken genaamd 'items' van alle losse woorden en integers
+    # uit 'data' zonder leestekens: '\x01', ',', '(', ')', '/'.
+    items = ' '.join([line.strip().lower() for line
+                      in data[1::3]]).replace('\x01', '')
 
-    items = ' '.join([line.strip()
-                      for line in data[1::3]]).replace(',', '').split()
+    for character in [',', '(', ')', '/', '[', ']']:
+        if character in items:
+            items = items.replace(character, ' ')
+    items = items.split()
 
     # lijst aanmaken genaamd 'geen_integers' met uitsluitend
     # woorden uit beschrijvingen.
-
     geen_integers = [woord for woord in items if not (
         woord.isdigit() or woord[0] == '-' and woord[1:].isdigit())]
 
     # lege dictionary aanmaken genaamd 'woord_freq'.
-
     woord_freq = {}
 
     # per woord uit 'geen_integers' de frequentie bepalen
     # en toevoegen aan 'woord_freq'.
-
     for woord in geen_integers:
         if woord not in woord_freq:
             woord_freq[woord] = 1
@@ -57,7 +57,6 @@ def TelWoorden(bestandsnaam):
             woord_freq[woord] += 1
 
     # sorteer dictionary 'woord_freq' op frequentie van laag naar hoog.
-
     woord_freq = {keys: values for keys, values in sorted(
         woord_freq.items(), key=lambda item: item[1])}
 
