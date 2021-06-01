85357f5b84a79f678aac25275fd95e384914b9ff
Noah Olmeijer <n.c.c.p.olmeijer@student.tue.nl>
2021-06-01 11:39:55 +0200

    Toevoegen van comments en aanpassen van variabelen.

--- Familie_Barchart.py
+++ Familie_Barchart.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def Histogram(cluster_uitvoer, FamilyCloneID):
+def Histogram(cluster_uitvoer, familie_cloneID, cluster_aantal):
     """Stacked bar chart van de distributie van families over de clusters.
 
     Parameters
@@ -19,7 +19,7 @@ def Histogram(cluster_uitvoer, FamilyCloneID):
     beschrijving: tekstbestand bevat per cloneID de relatieve
     expressiewaarden.
 
-    FamilyCloneID : tekstbestand
+    familie_cloneID : tekstbestand
     beschrijving: tekstbestand bevat per cloneID de corresponderende
     genfamilie.
 
@@ -29,73 +29,105 @@ def Histogram(cluster_uitvoer, FamilyCloneID):
     en de y-as de familiefrequentie.
 
     """
+    # inlezen van databestanden 'cluster_uitvoer' en 'familie_cloneID'.
     with open(cluster_uitvoer) as cluster_uitvoer:
-        cluster_uitvoer_data = cluster_uitvoer.read().split()
-        cluster_uitvoer = list(map(int, cluster_uitvoer_data))
+        cluster_uitvoer = cluster_uitvoer.read().split()
+        cluster_uitvoer = list(map(int, cluster_uitvoer))
 
-    with open(FamilyCloneID) as FamilyCloneID:
-        FamilyCloneID_data = FamilyCloneID.read().split()[2:]
-        FamilyCloneID = list(map(int, FamilyCloneID_data))
+    with open(familie_cloneID) as familie_cloneID:
+        familie_cloneID = familie_cloneID.read().split()[2:]
+        familie_cloneID = list(map(int, familie_cloneID))
 
-    Family_no = FamilyCloneID[1::2]
-    Family_cloneIDs = FamilyCloneID[::2]
+    # lijsten aanmaken van uitsluitend familienummers en cloneIDs.
+    familie_nummers = familie_cloneID[1::2]
+    familie_cloneIDs = familie_cloneID[::2]
 
-    dict1 = {}
+    # aanmaken van dictionary met als keys de familienummers en values
+    # een lege lijst.
+    familie_dict = {}
 
-    for i in range(1, 27):
-        dict1[i] = []
+    for familie_nummer in range(1, 27):
+        familie_dict[familie_nummer] = []
 
-    for Family in range(len(Family_no)):
-        dict1[Family_no[Family]].append(Family_cloneIDs[Family])
+    # alle cloneIDs toevoegen aan het corresponderende familienummer.
+    for Family in range(len(familie_nummers)):
+        familie_dict[familie_nummers[Family]].append(familie_cloneIDs[Family])
 
+    # lijsten aanmaken van uitsluitend clusternummers en cloneIDs.
     Cluster_no = cluster_uitvoer[1::2]
     Cluster_cloneIDs = cluster_uitvoer[::2]
 
-    dict2 = {}
+    # aanmaken van dictionary met als keys de clusternummers en values
+    # een lege lijst.
+    cluster_dict = {}
 
-    for i in range(0, 6):
-        dict2[i] = []
+    for cluster_nummer in range(0, cluster_aantal):
+        cluster_dict[cluster_nummer] = []
 
+    # alle cloneIDs toevoegen aan het corresponderende clusternummer.
     for Cluster in range(len(Cluster_no)):
-        dict2[Cluster_no[Cluster]].append(Cluster_cloneIDs[Cluster])
+        cluster_dict[Cluster_no[Cluster]].append(Cluster_cloneIDs[Cluster])
 
-    families = list(dict1.values())
-    clusters = list(dict2.values())
+    # lijsten aanmaken van alle cloneID lijsten uit beide dictionaries.
+    families = list(familie_dict.values())
+    clusters = list(cluster_dict.values())
 
-    dist_lst = []
+    distributies = []
 
-    for family in range(len(dict1)):
-        g = [0]*6
-        for cloneID in range(len(families[family])):
+    # loop over alle familie lijsten.
+    for familie in range(len(familie_dict)):
+        # lege distributie lijst aanmaken.
+        cur_dist = [0]*6
+        # loop over alle cloneIDs in de familielijst.
+        for cloneID in range(len(families[familie])):
+            # loop over elke cluster lijst.
             for cluster in range(len(clusters)):
-                if families[family][cloneID] in clusters[cluster]:
-                    g[cluster] += 1
-        dist_lst.append(g)
+                # check of cloneID uit familie lijst in cluster lijst zit.
+                if families[familie][cloneID] in clusters[cluster]:
+                    # verhoog cluster index met 1.
+                    cur_dist[cluster] += 1
+        # voeg distributielijst toe aan distributies.
+        distributies.append(cur_dist)
 
-    colors = ['#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
-              '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
-              '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
-              '#000075', '#808080', '#ffffff', '#aa11ab', '#eb5b01', '#db8c62',
-              '#f58b7a', '#288C00']
+    # definieer kleuren.
+    kleuren = [
+        '#e6194b', '#3cb44b', '#ffe119', '#4363d8', '#f58231', '#911eb4',
+        '#46f0f0', '#f032e6', '#bcf60c', '#fabebe', '#008080', '#e6beff',
+        '#9a6324', '#fffac8', '#800000', '#aaffc3', '#808000', '#ffd8b1',
+        '#000075', '#808080', '#ffffff', '#aa11ab', '#eb5b01', '#db8c62',
+        '#f58b7a', '#288C00']
 
-    i = 0
-    lst = np.array(dist_lst)
+    # omzetten van lijst naar numpy array en aanmaken van subplot.
+    distributies = np.array(distributies)
     fig, ax = plt.subplots(figsize=(10, 10))
-    ax.bar(range(6), lst[0], edgecolor='black', color=colors[0])
-    sum_arr = lst[0]
-    for data in lst[1:]:
-        i += 1
-        ax.bar(range(6), data, bottom=sum_arr, edgecolor='black',
-               color=colors[i])
-        sum_arr += data
 
-    ax.legend(list(range(1, len(lst))), title='families',
+    # eerste distributie toewijzen aan sum_arr.
+    sum_arr = distributies[0]
+
+    # aanmaken van stacked bar chart + plotten van eerste familie.
+    ax.bar(range(cluster_aantal),
+           sum_arr, edgecolor='black', color=kleuren[0])
+
+    iteratie = 0
+
+    # cumulatief plotten van elke distributie zodat een
+    # stacked bar chart ontstaat met per bar een andere kleur uit 'kleuren'.
+    for dist in distributies[1:]:
+        iteratie += 1
+        ax.bar(range(cluster_aantal), dist, bottom=sum_arr, edgecolor='black',
+               color=kleuren[iteratie])
+        sum_arr += dist
+
+    # toevoegen van labels die de grootte aangeven per bar.
+    for container in ax.containers:
+        labels = [label if label else '' for label in container.datavalues]
+        ax.bar_label(container, labels=labels, label_type='center', size=10)
+
+    # aanmaken van legenda.
+    ax.legend(list(range(1, len(distributies))), title='families',
               ncol=2, bbox_to_anchor=(1, 1))
 
-    for c in ax.containers:
-        labels = [a if a else '' for a in c.datavalues]
-        ax.bar_label(c, labels=labels, label_type='center', size=10)
-
+    # toevoegen van plot -en astitels.
     ax.set_title('Distributie van families over de clusters', size=15)
     ax.set_ylabel('Familiefrequentie', size=15)
     ax.set_xlabel('Clusters', size=15)
@@ -104,4 +136,4 @@ def Histogram(cluster_uitvoer, FamilyCloneID):
 cluster_uitvoer = "Data/Voorbeeld_clusterresult.txt"
 familie_no = "Data/CloneIdFamily.txt"
 
-Histogram(cluster_uitvoer, familie_no)
+Histogram(cluster_uitvoer, familie_no, 6)
